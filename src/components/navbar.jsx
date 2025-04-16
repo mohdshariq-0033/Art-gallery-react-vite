94c3d65c68f49e198b4635b5fa9a19683eab5f3b
mohd shariq <mohdshariq0033@gmail.com>
2025-04-16 21:40:34 +0530

    add git link in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,13 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Nav from "react-bootstrap/Nav";
+import { CgGitFork } from "react-icons/cg";
+import {AiFillStar} from "react-icons/ai";
+import Button from "react-bootstrap/Button";
 
 export default function Navbar() {
   useEffect(() => {
-    const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+    const navLinks = document.querySelectorAll(".navbar-nav .nav-top nav-link");
     const bsCollapse = document.getElementById("navbarSupportedContent");
 
     navLinks.forEach((link) => {
@@ -44,34 +48,34 @@ export default function Navbar() {
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/">
+              <Link className="nav-top nav-link" to="/">
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
+              <Link className="nav-top nav-link" to="/about">
                 About
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
+            <li className="nav-item" style={{marginRight:"10px"}}>
+              <Link className="nav-top nav-link" to="/contact">
                 Contact Us
               </Link>
             </li>
+            <Nav.Item className="fork-btn">
+              <Button
+                href="https://github.com/mohdshariq-0033/Art-gallery-react-vite"
+                target="_blank"
+                className="fork-btn-inner"
+                style={{height:"2.1em",marginTop:"3px"}}
+              >
+                <CgGitFork style={{ fontSize: "1.2em",marginBottom:"2em" }} />{" "}
+                <AiFillStar style={{ fontSize: "1.1em",marginBottom:"2em" }} />
+              </Button>
+            </Nav.Item>
           </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-primary" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
